34040b35fcc26bb0b8f651fcd226c2be9a4d7157
Aleksi Toivonen <aleksi.toivonen@tuni.fi>
2020-09-28 17:40:42 +0300

    New test

--- TOASApartmentWatch/ApartmentWatch.cs
+++ TOASApartmentWatch/ApartmentWatch.cs
@@ -88,7 +88,7 @@ namespace TOASApartmentWatch
 
             return currentApartments;
         }
-
+        // test
         private bool compareApartments(List<MonthlyApartmentContainer> data)
         {
             bool newApartmentDetected = false;
